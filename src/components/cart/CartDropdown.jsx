182deff3c1c1a8d4e971bee456ec715bc336d78e
Tony Keiser <keisertony@gmail.com>
2021-05-20 02:16:49 -0700

    Added reselect to app for cart selectors

--- src/components/cart/CartDropdown.jsx
+++ src/components/cart/CartDropdown.jsx
@@ -4,6 +4,7 @@ import './CartDropdown.scss'
 import Button from '../Buttons'
 import { connect } from 'react-redux'
 import CartItem from './CartItem'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 })
 
 export default connect(mapStateToProps)(CartDropdown)
